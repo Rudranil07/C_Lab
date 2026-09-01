f50a1a1f37627fbc093c60eddcdf1ffd4222dfb9
Rudranil07 <rudranilghoshrock@gmail.com>
2026-09-01 19:02:55 +0530

    okay

--- Patterns/cent_tri.c
+++ Patterns/cent_tri.c
@@ -5,17 +5,11 @@ int main(){
     for(i=0;i<5;i++)
     {
         for(j=0;j<-i+4;j++)
-        {
             printf(" ");
-        }
         for(j=-i+4;j<=4;j++)
-        {
             printf("*");
-        }
         for(j=i+4;j>4 && j<=8;j++)
-        {
             printf("*");
-        }
         printf("\n");
     }
     return 0;
